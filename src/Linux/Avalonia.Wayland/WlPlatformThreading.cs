c775f5a8b1cdb024050ca73666958485e59225b9
affederaffe <68356204+affederaffe@users.noreply.github.com>
2022-06-20 22:56:48 +0200

    Proper error handling in WlPlatformThreading.cs

--- src/Linux/Avalonia.Wayland/WlPlatformThreading.cs
+++ src/Linux/Avalonia.Wayland/WlPlatformThreading.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reactive.Disposables;
+using System.Runtime.InteropServices;
 using System.Threading;
 using Avalonia.FreeDesktop;
 using Avalonia.Platform;
@@ -15,6 +16,8 @@ namespace Avalonia.Wayland
         private readonly Thread _mainThread;
         private readonly Stopwatch _clock;
         private readonly List<ManagedThreadingTimer> _timers;
+        private readonly List<ManagedThreadingTimer> _readyTimers;
+        private readonly int _displayFd;
 
         public WlPlatformThreading(AvaloniaWaylandPlatform platform)
         {
@@ -22,59 +25,49 @@ namespace Avalonia.Wayland
             _mainThread = Thread.CurrentThread;
             _clock = Stopwatch.StartNew();
             _timers = new List<ManagedThreadingTimer>();
+            _readyTimers = new List<ManagedThreadingTimer>();
+            _displayFd = platform.WlDisplay.GetFd();
         }
 
         public unsafe void RunLoop(CancellationToken cancellationToken)
         {
             var pollFd = new pollfd
             {
-                fd = _platform.WlDisplay.GetFd(),
+                fd = _displayFd,
                 events = (int)(EpollEvents.EPOLLIN | EpollEvents.EPOLLHUP)
             };
 
-            var readyTimers = new List<ManagedThreadingTimer>();
             while (!cancellationToken.IsCancellationRequested)
             {
-                readyTimers.Clear();
-                var now = _clock.Elapsed;
-                TimeSpan nextTick = new(-1);
-                foreach (var timer in _timers)
-                {
-                    if (nextTick == new TimeSpan(-1) || timer.NextTick < nextTick)
-                        nextTick = timer.NextTick;
-                    if (timer.NextTick < now)
-                        readyTimers.Add(timer);
-                }
-
-                readyTimers.Sort();
-
-                foreach (var t in readyTimers)
-                {
-                    if (cancellationToken.IsCancellationRequested)
-                        return;
-                    t.Tick();
-                    if (t.Disposed)
-                        continue;
-                    t.Reschedule();
-                    if (nextTick == new TimeSpan(-1) || t.NextTick < nextTick)
-                        nextTick = t.NextTick;
-                }
+                var nextTick = DispatchTimers(cancellationToken);
 
                 while (_platform.WlDisplay.PrepareRead() != 0)
                     _platform.WlDisplay.DispatchPending();
-                _platform.WlDisplay.Flush();
 
-                if (cancellationToken.IsCancellationRequested)
-                    return;
+                if (cancellationToken.IsCancellationRequested || !FlushDisplay())
+                {
+                    _platform.WlDisplay.CancelRead();
+                    break;
+                }
 
                 var timeout = nextTick == new TimeSpan(-1) ? -1 : Math.Max(1, (int)(nextTick - _clock.Elapsed).TotalMilliseconds);
                 var ret = NativeMethods.poll(&pollFd, new IntPtr(1), timeout);
 
-                if (cancellationToken.IsCancellationRequested || ret <= 0)
+                if (cancellationToken.IsCancellationRequested || ret < 0)
+                {
                     _platform.WlDisplay.CancelRead();
+                    break;
+                }
 
-                if (ret > 0)
+                if ((pollFd.revents & (int)EpollEvents.EPOLLIN) > 0)
+                {
                     _platform.WlDisplay.ReadEvents();
+                    _platform.WlDisplay.DispatchPending();
+                }
+                else
+                {
+                    _platform.WlDisplay.CancelRead();
+                }
 
                 Dispatcher.UIThread.RunJobs();
             }
@@ -98,5 +91,58 @@ namespace Avalonia.Wayland
         public bool CurrentThreadIsLoopThread => Thread.CurrentThread == _mainThread;
 
         public event Action<DispatcherPriority?>? Signaled;
+
+        private TimeSpan DispatchTimers(CancellationToken cancellationToken)
+        {
+            _readyTimers.Clear();
+            var now = _clock.Elapsed;
+            TimeSpan nextTick = new(-1);
+            foreach (var timer in _timers)
+            {
+                if (nextTick == new TimeSpan(-1) || timer.NextTick < nextTick)
+                    nextTick = timer.NextTick;
+                if (timer.NextTick < now)
+                    _readyTimers.Add(timer);
+            }
+
+            _readyTimers.Sort();
+
+            foreach (var t in _readyTimers)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                    break;
+                t.Tick.Invoke();
+                if (t.Disposed)
+                    continue;
+                t.Reschedule();
+                if (nextTick == new TimeSpan(-1) || t.NextTick < nextTick)
+                    nextTick = t.NextTick;
+            }
+
+            return nextTick;
+        }
+
+        private unsafe bool FlushDisplay()
+        {
+            while (_platform.WlDisplay.Flush() == -1)
+            {
+                if (Marshal.GetLastWin32Error() != (int)Errno.EAGAIN)
+                    return false;
+
+                var pollFd = new pollfd
+                {
+                    fd = _displayFd,
+                    events = (int)EpollEvents.EPOLLOUT
+                };
+
+                while (NativeMethods.poll(&pollFd, new IntPtr(1), -1) == -1)
+                {
+                    if (Marshal.GetLastWin32Error() is not (int)Errno.EINTR and not (int)Errno.EAGAIN)
+                        return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
