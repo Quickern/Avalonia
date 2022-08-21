70f8afb2a2bb4ef7d4a05936e1de447711475525
affederaffe <68356204+affederaffe@users.noreply.github.com>
2022-08-21 20:23:31 +0200

    Don't throw when missing interface is not required

--- src/Linux/Avalonia.Wayland/WlRegistryHandler.cs
+++ src/Linux/Avalonia.Wayland/WlRegistryHandler.cs
@@ -40,7 +40,8 @@ namespace Avalonia.Wayland
 
         public void OnGlobalRemove(WlRegistry eventSender, uint name)
         {
-            if (!_globals.TryGetValue(name, out var glob)) return;
+            if (!_globals.TryGetValue(name, out var glob))
+                return;
             _globals.Remove(name);
             GlobalRemoved?.Invoke(glob);
         }
@@ -54,11 +55,7 @@ namespace Avalonia.Wayland
         public T? Bind<T>(IBindFactory<T> factory, string @interface, int version) where T : WlProxy
         {
             var global = _globals.Values.FirstOrDefault(g => g.Interface == @interface);
-
-            if (global is null)
-                throw new NotSupportedException($"Unable to find {@interface} in the registry");
-
-            return Bind(factory, version, global);
+            return global is null ? null : Bind(factory, version, global);
         }
 
         public unsafe T? Bind<T>(IBindFactory<T> factory, int version, GlobalInfo global) where T : WlProxy
@@ -76,16 +73,18 @@ namespace Avalonia.Wayland
 
         public sealed class GlobalInfo
         {
-            public uint Name { get; }
-            public string Interface { get; }
-            public int Version { get; }
-
             internal GlobalInfo(uint name, string @interface, int version)
             {
                 Name = name;
                 Interface = @interface;
                 Version = version;
             }
+
+            public uint Name { get; }
+
+            public string Interface { get; }
+
+            public int Version { get; }
 
             public override string ToString() => $"{Interface} version {Version} at {Name}";
         }
